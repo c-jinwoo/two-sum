7fca97bb76027264585180259d8a62cddcd497a8
c-jinwoo <jihn011258@naver.com>
2023-03-09 19:11:40 +0900

    Week02 Assignment

---
+++ main.py
@@ -0,0 +1,22 @@
+"""
+    강의 : 오픈소스소프트웨어실습
+    날짜 : 2023.03.09
+    내용 : 2주차 과제
+"""
+
+def twoSum(nums: list[int], target: int) -> list[int]:
+    ans = []
+
+    for i in (0, len(nums) - 2):
+        for j in (i + 1, len(nums) - 1):
+            if nums[i] + nums[j] == target:
+                ans.append(i)
+                ans.append(j)
+                break
+
+        if len(ans) > 0:
+            break
+
+    return ans
+
+    
